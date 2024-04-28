5327a804cc8b430f1de074b5c37b7d51922e5bcb
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-04-29 04:36:49 +0600

    soacial login & spinner added

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+	return (
+		<div className='flex justify-center items-center w-full h-full'>
+			<span className='loading loading-spinner text-[#29d9d5] w-40'></span>
+		</div>
+	)
+}
+
+export default Spinner
