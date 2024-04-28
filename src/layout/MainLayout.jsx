f338bb3310bff059fdea8f3c5f8e73833484e026
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-04-29 04:38:29 +0600

    main layout added

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+// import Footer from "../components/Footer"
+
+const MainLayout = () => {
+	return (
+		<>
+			<div className='container mx-auto'>
+				<Navbar />
+				<Outlet />
+			</div>
+			<Footer />
+		</>
+	)
+}
+
+export default MainLayout
